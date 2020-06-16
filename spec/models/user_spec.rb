63f8f70317ba798e2f3e6f5fda20b4cbbc5d3cb4
kbjude <kbjude@gmail.com>
2020-06-16 20:55:14 +0300

    fixing gem error

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'Login attemps' do
+    it 'returns login form when no user is logged in' do
+      visit new_user_session_path
+      expect(page).to have_content('Sign in')
+    end
+
+    it 'User is redirected to the index page' do
+      visit root_path
+      expect(page).to have_content('Recent posts')
+    end
+  end
+end
