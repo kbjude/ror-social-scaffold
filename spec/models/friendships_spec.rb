0c472e963acfc49ead479523e2c06bfc128e99ba
kbjude <kbjude@gmail.com>
2020-06-18 11:09:08 +0300

    friendship specs complete

--- spec/models/friendships_spec.rb
+++ spec/models/friendships_spec.rb
@@ -1,10 +1,10 @@
 RSpec.describe 'Friendship', type: :model do
     describe 'Friendship creation' do
-        it ' Fails when friend id is abscent' do
+        it ' Returns the last added friend' do
         user = User.create(name: "Jude", email: "kk7@gmail.com", password: "Heaven")
-        user2 = User.create(name: "Jude", email: "kbg@gmail.com", password: "Heaven")
-        f = user.friendships.create(user_id: user.id, friend_id: user2.id, confirmed:false)
-        expect(f).to eql(false)
+        user2 = User.create(name: "Jude", email: "kk7@gmail.com", password: "Heaven")
+        f = user.friendships.create(user_id: user.id, friend_id: user2.id, confirmed:true)
+        expect(user.friendships.last).to eql(f)
         end
     end
 end
