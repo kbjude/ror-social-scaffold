70bc2d62d676bc702b8c339e0cb314841d9e1669
kbjude <kbjude@gmail.com>
2020-06-19 15:42:56 +0300

    linter errors fixed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show]
   before_action :authenticate_user!
-  before_action :authenticate_user!, only: [:show, :index]
+  before_action :authenticate_user!, only: %i[show index]
 
   def index
-      @post = Post.new
-      timeline_posts
+    @post = Post.new
+    timeline_posts
   end
 
   def create
@@ -28,7 +28,8 @@ class PostsController < ApplicationController
   def timeline_posts
     if current_user
       @timeline_posts ||= if current_user
-                            Post.where(user: current_user.friends && [current_user]).ordered_by_most_recent.includes(:user)
+                            @pos = Post.where(user: current_user.friends && [current_user])
+                            @pos.ordered_by_most_recent.includes(:user)
                           else
                             Post.all.ordered_by_most_recent.includes(:user)
                           end
