040956faba8f7e16dbd18aa0344343c8cdd5b657
kbjude <kbjude@gmail.com>
2020-06-23 22:51:56 +0300

    spec

--- spec/helpers/friendships_helper_spec.rb
+++ spec/helpers/friendships_helper_spec.rb
@@ -6,11 +6,4 @@ RSpec.describe Friendship, type: :model do
     expect(friendship.macro).to eq(:belongs_to)
   end
 
-  context 'Mutual friend' do
-    let(:user1) {User.create(name: 'Lillian', email: 'lil@gmail.com', password: 'blessing')}
-    let(:user2) {User.create(name: 'Esie', email: 'esther@gmail.com', password: 'blessing')}
-    let(:user3) {User.create(name: 'Jude', email: 'jude@gmail.com', password: 'ChristLord')}
-
-
-  end
 end
