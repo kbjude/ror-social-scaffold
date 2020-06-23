e67099c072181161efa58254bd789168224632e9
kbjude <kbjude@gmail.com>
2020-06-23 22:28:58 +0300

    mutual tests

--- spec/helpers/friendships_helper_spec.rb
+++ spec/helpers/friendships_helper_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Friendship, type: :model do
     friendship = Friendship.reflect_on_association(:user)
     expect(friendship.macro).to eq(:belongs_to)
   end
+
+  context 'Mutual friend' do
+    let(:user1) {User.create(name: 'Lillian', email: 'lil@gmail.com', password: 'blessing')}
+    let(:user2) {User.create(name: 'Esie', email: 'esther@gmail.com', password: 'blessing')}
+    let(:user3) {User.create(name: 'Jude', email: 'jude@gmail.com', password: 'ChristLord')}
+
+    it 'is when two friends a common friend' do
+      user1.friendships.create(user_id: user1.id, friend_id: user2.id, confirmed: true)
+      user2.friendships.create(user_id: user2.id, friend_id: user3.id, confirmed: true)
+      user1.friendships.create(user_id: user3.id, friend_id: user3.id, confirmed: true)
+      expect(user1.friendships.first.mutual).to eql([user3])
+    end
+  end
 end
