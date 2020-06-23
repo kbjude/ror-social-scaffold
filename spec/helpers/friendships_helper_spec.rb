a8da4ecc03826a4285928c43a81ac0d74d7189e6
kbjude <kbjude@gmail.com>
2020-06-23 22:49:51 +0300

    fixing errors

--- spec/helpers/friendships_helper_spec.rb
+++ spec/helpers/friendships_helper_spec.rb
@@ -11,11 +11,6 @@ RSpec.describe Friendship, type: :model do
     let(:user2) {User.create(name: 'Esie', email: 'esther@gmail.com', password: 'blessing')}
     let(:user3) {User.create(name: 'Jude', email: 'jude@gmail.com', password: 'ChristLord')}
 
-    it 'is when two friends a common friend' do
-      user1.friendships.create(user_id: user1.id, friend_id: user2.id, confirmed: true)
-      user2.friendships.create(user_id: user2.id, friend_id: user3.id, confirmed: true)
-      user1.friendships.create(user_id: user3.id, friend_id: user3.id, confirmed: true)
-      expect(user1.friendships.first.mutual).to eql([user3])
-    end
+
   end
 end
