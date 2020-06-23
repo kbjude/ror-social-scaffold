56a266985d200f44d2ef245be65d5f6dac6b90a2
kbjude <kbjude@gmail.com>
2020-06-23 19:49:40 +0300

    fixes

--- spec/helpers/friendships_helper_spec.rb
+++ spec/helpers/friendships_helper_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-    it 'has to be for a user' do
-      friendship = Friendship.reflect_on_association(:user)
-      expect(friendship.macro).to eq(:belongs_to)
-    end
+  it 'has to be for a user' do
+    friendship = Friendship.reflect_on_association(:user)
+    expect(friendship.macro).to eq(:belongs_to)
+  end
 end
